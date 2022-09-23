af6b58a1cbe70151dc8cf4487e68f4ef22762d95
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-09-23 17:13:12 -0300

    crud colaboradores(usuarios) pra tela admin

--- 2RP/src/controllers/ColaboradorController.ts
+++ 2RP/src/controllers/ColaboradorController.ts
@@ -47,4 +47,47 @@ export default class ColaboradorController {
         }
     }
 
+    async salvarColaborador(req: Request, res: Response) {
+        const { nome, matricula, turno, email, telefone, perfil, cr } = req.body
+
+        try {
+            const novoColaborador = AppDataSource.manager.create(Colaborador, { nome, matricula, turno, email, telefone, perfil, cr })
+            await AppDataSource.manager.save(Colaborador, novoColaborador)
+
+            return res.json(novoColaborador)
+        } catch (error) {
+            console.log(error)
+            return res.json({message: "Internal Server Error"})
+        }
+    }
+
+    async atualizarColaborador(req: Request, res: Response) {
+        const { id } = req.params
+        const { nome, matricula, turno, email, telefone, perfil, cr } = req.body
+
+        try {
+            const novoColaborador = AppDataSource.manager.create(Colaborador, { id: Number(id), nome, matricula, turno, email, telefone, perfil, cr })
+            await AppDataSource.manager.save(Colaborador, novoColaborador)
+
+            return res.json(novoColaborador)
+        } catch (error) {
+            console.log(error)
+            return res.json({message: "Internal Server Error"})
+        }
+    }
+
+    async excluirColaborador(req: Request, res: Response) {
+        const { id } = req.params
+        
+        try {
+            const colaborador = await AppDataSource.manager.findOneBy(Colaborador, { id: Number(id) })
+            await AppDataSource.manager.delete(Colaborador, colaborador)
+
+            return res.json(colaborador)
+        } catch (error) {
+            console.log(error)
+            return res.json({message: "Internal Server Error"})
+        }
+    }
+
 }
--- 2RP/src/routes/routesColaborador.ts
+++ 2RP/src/routes/routesColaborador.ts
@@ -5,5 +5,8 @@ const routesColaborador = Router()
 
 routesColaborador.get('/selectColaboradores', new ColaboradorController().selectColaboradores)
 routesColaborador.get('/selectGestores', new ColaboradorController().selectGestores )
+routesColaborador.post('/salvarColaborador', new ColaboradorController().salvarColaborador )
+routesColaborador.put('/atualizarColaborador/:id', new ColaboradorController().atualizarColaborador )
+routesColaborador.delete('/excluirColaborador/:id', new ColaboradorController().excluirColaborador )
 
 export default routesColaborador
